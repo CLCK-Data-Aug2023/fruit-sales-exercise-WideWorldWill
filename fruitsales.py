bd49d1b562398e2d59dff538b24625a86727d1c1
WideWorldWill <142928659+WideWorldWill@users.noreply.github.com>
2023-12-17 14:05:48 -0500

    Add files via upload

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,10 @@
-# add your code here
+import csv
+
+filename = "fruit.csv"
+
+with open(filename, "w", newline="") as csvfile:
+  writer = csv.writer(csvfile, dialect="excel")
+
+  writer.writerow(["", "Apples", "Bananas"])
+  writer.writerow(["2017 Sales", "35", "21"])
+  writer.writerow(["2018 Sales", "41", "34"])
